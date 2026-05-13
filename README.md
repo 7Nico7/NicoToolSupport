5c192c57b7b3f60699728a5152810984546210b6
Nico <nicolasarcos.ov@gmail.com>
2026-05-13 16:29:19 -0600

    Nuevo provedor websocket pusher

--- README.md
+++ README.md
@@ -58,5 +58,9 @@ If you discover a security vulnerability within Laravel, please send an e-mail t
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
 
+//server
+composer require pusher/pusher-php-server
 
+
+//local
 levantar websocket php artisan reverb:start
--- resources/js/echo.js
+++ resources/js/echo.js
@@ -5,6 +5,9 @@
 // VERIFICA que tenga exactamente esta estructura para Reverb.
 // Las variables VITE_REVERB_* se añaden a .env por el mismo comando.
 
+/*
+
+local sin depender de cuentas se requiere servidor vps con dominio y certificado ssl
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
@@ -18,4 +21,20 @@ window.Echo = new Echo({
     wssPort:            import.meta.env.VITE_REVERB_PORT  ?? 443,
     forceTLS:           (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports:  ['ws', 'wss'],
+});
+ */
+
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+
+    forceTLS: true,
 });
